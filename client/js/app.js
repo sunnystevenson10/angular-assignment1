e6b1d2906296370a6f54ddbc488aaf9b30c1f0b2
sunnystevenson10 <sunnystevenson10@gmail.com>
2015-10-01 08:21:29 -0600

    Assignment 1.5

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,55 @@
 angular.module('RepsApp', [
-  'RepsAppControllers'
+'RepsAppControllers'
+
 ]);
 
+
 angular
-  .module('RepsAppControllers', [
-    'repsService'
-  ])
+.module('RepsAppControllers', [
+  'repsService'
+])
   .controller('MainCtrl', function (reps) {
     var main = this;
+    main.loading = false;
     main.reps = [];
+    main.congressType = 'reps';
+    main.loading = false;
+    main.apis = [
+      {
+        label: 'Zip',
+        method: function (zip){
+          main.loading = true;
+          reps('all', 'zip', zip).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'Last Name',
+        method: function (name){
+          main.loading = true;
+          reps(main.congressType, 'name', name).then(function (data){
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'State',
+        method: function (state){
+          main.loading = true;
+          reps(main.congressType, 'state', state).then(function (data){
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      }
+    ];
+  main.criteria = main.apis[0];
+/*
 
-    main.searchByZip = function (zip) {
+     main.searchByZip = function (zip) {
       reps.allByZip(zip).then(function (data) {
         main.reps = data;
       });
@@ -21,45 +60,70 @@ angular
         main.reps = data;
       });
     };
+
     main.searchRepsByState = function (state) {
       reps.repsByState(state).then(function (data) {
         main.reps = data;
       });
     };
-    main.searchSensByName = function (senName) {
-      reps.sensByName(senName).then(function (data) {
+
+    main.searchSensByName = function (name) {
+      reps.sensByName(name).then(function (data) {
         main.reps = data;
       });
     };
+
     main.searchSensByState = function (senState) {
       reps.sensByState(senState).then(function (data) {
         main.reps = data;
       });
     };
 
+    **/
   });
 
+
+
+
 angular
-  .module('repsService', [])
-  .factory('reps', function ($http) {
+  .module ('repsService', [])
+  .factory('reps', function($http) {
     var host = 'http://dgm-representatives.herokuapp.com';
+
+
+    function search(type, criteria, query) {
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + '/' + query)
+        .then(function (response){
+          return response.data;
+        });
+    }
+
+   // search.allByZip = search.bind(null, 'all', 'zip');
+  //  search.repsByName = search.bind(null, 'reps', 'name');
+  //  search.repsByState = search.bind(null, 'reps', 'state');
+  //  search.sensByName = search.bind(null, 'sens', 'name');
+ //   search.sensByState = search.bind(null, 'sens', 'state');
+
+
+    return search;
+  });
+/**
     return {
-      allByZip: function (zip) {
+      allByZip: function (zip){
         return $http
-          .get(host + '/all/by-zip/' + zip)
+          .get(host +'/all/by-zip/' + zip)
           .then(function (response) {
             return response.data;
           });
       },
-
       repsByName: function (name) {
         return $http
-          .get(host + '/reps/by-name/' + name)
-          .then(function (response) {
-            return response.data;
-          });
+        .get(host + '/reps/by-name/' + name)
+        .then(function (response) {
+          return response.data;
+        });
       },
-
       repsByState: function (state) {
         return $http
         .get(host + '/reps/by-state/' + state)
@@ -67,7 +131,6 @@ angular
           return response.data;
         });
       },
-
       sensByName: function (name) {
         return $http
         .get(host + '/sens/by-name/' + name)
@@ -75,14 +138,15 @@ angular
           return response.data;
         });
       },
-
       sensByState: function (state) {
         return $http
         .get(host + '/sens/by-state/' + state)
         .then(function (response) {
           return response.data;
         });
-      },
-
+      }
     };
+
   });
+
+*/
